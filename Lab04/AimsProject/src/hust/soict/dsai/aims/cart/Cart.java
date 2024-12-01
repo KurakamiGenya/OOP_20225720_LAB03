b7548a0a77a36f2e9b4a72039a6b52a92dfca9ad
Nguyễn Minh Hoàng <macarong0986711447@gmail.com>
2024-12-01 20:29:13 +0700

    2nd part

--- Lab04/AimsProject/src/hust/soict/dsai/aims/cart/Cart.java
+++ Lab04/AimsProject/src/hust/soict/dsai/aims/cart/Cart.java
@@ -2,7 +2,7 @@ package hust.soict.dsai.aims.cart;
 
 import java.util.ArrayList;
 
-import hust.soict.dsai.aims.media.DigitalVideoDisc;
+//import hust.soict.dsai.aims.media.DigitalVideoDisc;
 import hust.soict.dsai.aims.media.Media;
 
 public class Cart {
@@ -11,11 +11,29 @@ public class Cart {
 
     private ArrayList<Media> itemsOrdered = new ArrayList<Media>();
 
+    public void addMedia(Media media) {
+        if (!itemsOrdered.contains(media)) {
+            itemsOrdered.add(media);
+            System.out.println("The item has been added to your cart.");
+        } else {
+            System.out.println("The item is already in your cart.");
+        }
+    }
+
+    public void removeMedia(Media media) {
+        if (itemsOrdered.contains(media)) {
+            itemsOrdered.remove(media);
+            System.out.println("The item has been removed from your cart.");
+        } else {
+            System.out.println("The item is not in your cart to remove.");
+        }
+    }
+
     // Method to calculate the total cost of all DVDs in the cart
     public float totalCost() {
         float total = 0;
-        for (Media disc : itemsOrdered) {
-            total += disc.getCost();
+        for (Media media : itemsOrdered) {
+            total += media.getCost();
         }
         return total;
     }
@@ -31,70 +49,81 @@ public class Cart {
      * }
      */
 
-    public void displayCart() {
-        System.out.println("***********************CART***********************");
-        System.out.println("Ordered Items:");
-        int index = 1;
-        for (Media disc : itemsOrdered) {
-            System.out.printf("%d. DVD - %s - %s - %s - %d: %.2f $\n",
-                    index++,
-                    disc.getTitle(),
-                    disc.getCategory(),
-                    disc.getDirector(),
-                    disc.getLength(),
-                    disc.getCost());
-        }
-        System.out.printf("Total cost: %.2f\n", totalCost());
-        System.out.println("****************************************************");
-    }
+    /*
+     * public void displayCart() {
+     * System.out.println("***********************CART***********************");
+     * System.out.println("Ordered Items:");
+     * int index = 1;
+     * for (Media media : itemsOrdered) {
+     * System.out.printf("%d. DVD - %s - %s - %s - %d: %.2f $\n",
+     * index++,
+     * media.getTitle(),
+     * media.getCategory(),
+     * media.getDirector(),
+     * media.getLength(),
+     * media.getCost());
+     * }
+     * System.out.printf("Total cost: %.2f\n", totalCost());
+     * System.out.println("****************************************************");
+     * }
+     */
 
-    public void findDVDById(int id) {
-        boolean found = false;
-        for (Media disc : itemsOrdered) {
-            if (disc.getId() == id) {
-                System.out.println("DVD found:");
-                System.out.printf("ID: %d - Title: %s - Category: %s - Director: %s - Length: %d - Price: %.2f $\n",
-                        disc.getId(),
-                        disc.getTitle(),
-                        disc.getCategory(),
-                        disc.getDirector(),
-                        disc.getLength(),
-                        disc.getCost());
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            System.out.println("No DVD found with ID: " + id);
-        }
-    }
-
-    public void findDVDByTitle(String keywords) {
-        String[] keywordArray = keywords.toLowerCase().split("\\s+"); // Convert the string of keywords into an array of
-                                                                      // separate words, and split them with space
-                                                                      // characters
-        boolean found = false;
-
-        System.out.println("Search results for keywords: \"" + keywords + "\":");
-        for (Media disc : itemsOrdered) {
-            String titleLower = disc.getTitle().toLowerCase(); // Convert the title to lowercase
-            for (String keyword : keywordArray) {
-                if (titleLower.contains(keyword)) {
-                    System.out.printf("ID: %d - Title: %s - Category: %s - Director: %s - Length: %d - Price: %.2f $\n",
-                            disc.getId(),
-                            disc.getTitle(),
-                            disc.getCategory(),
-                            disc.getDirector(),
-                            disc.getLength(),
-                            disc.getCost());
-                    found = true;
-                    break; // Stop checking with other keywords if one has been matched
-                }
-            }
-        }
-        if (!found) {
-            System.out.println("No DVD found matching the keywords: \"" + keywords + "\"");
-        }
-    }
+    /*
+     * public void findDVDById(int id) {
+     * boolean found = false;
+     * for (Media disc : itemsOrdered) {
+     * if (disc.getId() == id) {
+     * System.out.println("DVD found:");
+     * System.out.
+     * printf("ID: %d - Title: %s - Category: %s - Director: %s - Length: %d - Price: %.2f $\n"
+     * ,
+     * disc.getId(),
+     * disc.getTitle(),
+     * disc.getCategory(),
+     * disc.getDirector(),
+     * disc.getLength(),
+     * disc.getCost());
+     * found = true;
+     * break;
+     * }
+     * }
+     * if (!found) {
+     * System.out.println("No DVD found with ID: " + id);
+     * }
+     * }
+     * 
+     * public void findDVDByTitle(String keywords) {
+     * String[] keywordArray = keywords.toLowerCase().split("\\s+"); // Convert the
+     * string of keywords into an array of
+     * // separate words, and split them with space
+     * // characters
+     * boolean found = false;
+     * 
+     * System.out.println("Search results for keywords: \"" + keywords + "\":");
+     * for (Media disc : itemsOrdered) {
+     * String titleLower = disc.getTitle().toLowerCase(); // Convert the title to
+     * lowercase
+     * for (String keyword : keywordArray) {
+     * if (titleLower.contains(keyword)) {
+     * System.out.
+     * printf("ID: %d - Title: %s - Category: %s - Director: %s - Length: %d - Price: %.2f $\n"
+     * ,
+     * disc.getId(),
+     * disc.getTitle(),
+     * disc.getCategory(),
+     * disc.getDirector(),
+     * disc.getLength(),
+     * disc.getCost());
+     * found = true;
+     * break; // Stop checking with other keywords if one has been matched
+     * }
+     * }
+     * }
+     * if (!found) {
+     * System.out.println("No DVD found matching the keywords: \"" + keywords +
+     * "\"");
+     * }
+     * }
+     */
 
 }
